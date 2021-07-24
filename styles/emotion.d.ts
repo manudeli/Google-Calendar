1bab5e2d81681e30620ccd174905ed5e54008ce1
Jonghyeon Ko <flowithusdev@gmail.com>
2021-07-24 22:20:41 +0900

    Create theme material colors

--- styles/emotion.d.ts
+++ styles/emotion.d.ts
@@ -1,22 +1,47 @@
 import '@emotion/react';
 
 declare module '@emotion/react' {
+  export interface IColorNumber {
+    '50': string;
+    '100': string;
+    '200': string;
+    '300': string;
+    '400': string;
+    '500': string;
+    '600': string;
+    '700': string;
+    '800': string;
+    '900': string;
+    a100: string;
+    a200: string;
+    a400: string;
+    a700: string;
+  }
+
+  export interface IColor {
+    red: IColorNumber;
+    pink: IColorNumber;
+    purple: IColorNumber;
+    deeppurple: IColorNumber;
+    indigo: IColorNumber;
+    blue: IColorNumber;
+    lightblue: IColorNumber;
+    cyan: IColorNumber;
+    teal: IColorNumber;
+    green: IColorNumber;
+    lightgreen: IColorNumber;
+    lime: IColorNumber;
+    yellow: IColorNumber;
+    amber: IColorNumber;
+    orange: IColorNumber;
+    deeporange: IColorNumber;
+    brown: IColorNumber;
+    grey: IColorNumber;
+    bluegrey: IColorNumber;
+  }
+
   export interface Theme {
-    color: {
-      blue: {
-        default: string;
-        50: string;
-        100: string;
-        200: string;
-        300: string;
-        400: string;
-        500: string;
-        600: string;
-        700: string;
-        800: string;
-        900: string;
-      };
-    };
+    color: IColor;
     mediaQuery: {
       laptop: string;
       tablet: string;
