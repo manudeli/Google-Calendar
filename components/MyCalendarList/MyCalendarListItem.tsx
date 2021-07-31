345785e30806159cca175a52870f3118f937992f
Jonghyeon Ko <flowithusdev@gmail.com>
2021-07-31 23:33:04 +0900

    Add calendarList hovered Icons

--- components/MyCalendarList/MyCalendarListItem.tsx
+++ components/MyCalendarList/MyCalendarListItem.tsx
@@ -6,6 +6,7 @@ import { CalendarId } from '../../models/types';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import theme from '../../styles/theme';
 import CheckBox from '../UI/CheckBox';
+import IconButton from '../UI/IconButton';
 
 interface Props {
   calendar: CalendarProps;
@@ -13,6 +14,8 @@ interface Props {
 }
 
 const MyCalendarListItem = ({ calendar, isDisplay }: Props) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   const currentUserId = useAppSelector((state) => state.user.profile.id);
   const dispatch = useAppDispatch();
 
@@ -31,18 +34,48 @@ const MyCalendarListItem = ({ calendar, isDisplay }: Props) => {
       css={css`
         display: flex;
         gap: 4px;
+        justify-content: space-between;
         align-items: center;
         cursor: pointer;
-        padding: 6px 17px;
+        padding: 0 17px;
         &:hover {
           background: ${theme.color.grey[200]};
         }
         border-radius: ${theme.radiusSize.small};
       `}
       onClick={() => clickListItemHandle(calendar.id)}
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}
     >
-      <CheckBox checked={isDisplay} />
-      <div>{calendar.title}</div>
+      <div
+        css={css`
+          padding: 6px 0;
+          display: flex;
+          align-items: center;
+        `}
+      >
+        <CheckBox checked={isDisplay} />
+        <div>{calendar.title}</div>
+      </div>
+      {isHovered && (
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+          `}
+        >
+          <IconButton
+            size={23}
+            icon="close"
+            tooltip={`Unsubscribe from ${calendar.title}`}
+          />
+          <IconButton
+            size={23}
+            icon="more_vert"
+            tooltip={`Options for ${calendar.title}`}
+          />
+        </div>
+      )}
     </li>
   );
 };
